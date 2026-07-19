df3085f7fffb5bd1fd3b2be5cfba9e2800555829
Mrinal Kadam <mrinalkadam09@gmail.com>
2026-07-19 23:57:37 +0530

    updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,8 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-
 import App from "./App.jsx"
-import "./index.css"
 import { AuthProvider } from "./context/AuthContext.jsx"
+import "./index.css"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
