a05288494f4a34dd5380cbd983343df2d39e6cf5
Mrinal Kadam <mrinalkadam09@gmail.com>
2026-08-03 20:24:36 +0530

    updated with new versions

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base,sessionmaker
-import os
+from sqlalchemy.orm import declarative_base, sessionmaker
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
@@ -16,6 +16,7 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
